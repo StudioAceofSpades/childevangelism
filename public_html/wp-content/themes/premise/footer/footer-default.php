703b87488abc235aab150c3dea2b9dd6c9a0d661
SrishtiAvalakki <spram001@odu.edu>
2020-12-29 22:04:08 +0000

    Footer completed

--- public_html/wp-content/themes/premise/footer/footer-default.php
+++ public_html/wp-content/themes/premise/footer/footer-default.php
@@ -6,6 +6,16 @@
             <div class = "col-sm">
                 <h3>About Child Evangelism Ministries</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.</p>
+                <p class = "copyright">
+                    <a href="#" class="fb">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/fb.png" class="img-fluid">
+                    </a>
+                    <a href="#" class="subtract">
+                        <img src="<?php bloginfo('template_directory'); ?>/img/Subtract.png" class="img-fluid">
+                    </a>
+                    © 2020 Child Evangelism Ministries
+                </p>
+
             </div>
             <div class = "col-sm">
                 <div class = "nav">
