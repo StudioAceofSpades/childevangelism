e47fe87a8e5689e9ea851d08e3b8b79b3d2b5488
SrishtiAvalakki <spram001@odu.edu>
2021-01-04 20:45:17 +0000

    2nd quarter body

--- public_html/wp-content/themes/premise/pages/home.php
+++ public_html/wp-content/themes/premise/pages/home.php
@@ -31,6 +31,38 @@ get_header(); ?>
                     </div>
                 </div>
             </div>
+            <img class="hero" src="<?php bloginfo('template_directory'); ?>/img/hero.png">
+            <h3>Children's Stories</h3>
+            <div class="children row">
+                <div class="col-sm text-center">
+                    <div class="img">
+                        <img class="sam" src="<?php bloginfo('template_directory'); ?>/img/sam.png">
+                        <img class="esther" src="<?php bloginfo('template_directory'); ?>/img/esther.png">
+                        <img class="secret" src="<?php bloginfo('template_directory'); ?>/img/secret.png">
+                    </div>
+                </div>
+            </div>
+            <div class="row secondary">
+                <div class="col-sm text-center">
+                    <div class="secondary">
+                        <a href="#" class="secondary Yellow">A Miracle for Samuelito</a>
+                        <a href="#" class="secondary purple">The Life of Esther</a>
+                        <a href="#" class="secondary Blue">Secret in the Woods</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row title">
+                <div class = "col-sm">
+                    <h2>Happy Day Club 2020 - 2021</h2>
+                    <h3>Although Happy Day Club isn't able to meet in your school this year we have new ways to enjoy Happy Day Club!</h3>
+                    <p>Happy Day Clubs are after school Bible clubs meeting once a week for one hour from October through March. Happy Day Clubs are taught by teams of trained volunteers. Each week children are taught a Bible lesson, hear the next chapter of an exciting missionary story, sing songs, learn a Bible verse and so much more! </p>
+                </div>
+                <div class = "col">
+                    <a href="#" class="club">About Happy Day Club</a>
+                </div>
+            </div>
+            <h1>next section</h1>
+
         </div>
     </section>
 </div>
