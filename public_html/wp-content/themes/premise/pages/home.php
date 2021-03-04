57365038dc2d3a8da41815d0a1d75310004a15d9
SrishtiAvalakki <spram001@odu.edu>
2021-03-04 23:31:31 +0000

    Revisions Updated

--- public_html/wp-content/themes/premise/pages/home.php
+++ public_html/wp-content/themes/premise/pages/home.php
@@ -10,38 +10,34 @@ get_header(); ?>
     <div id="home">
         <section class="bg-white">
             <div class = "container">
-                <div class = "backer">
-                    <div class = "incontainer">
-                        <div class = "bcard">
-                            <div class="row">
-                                <div class = "col text-center">
-                                    <h2 class="head">Welcome to Child Evangelism Ministries</h2>
-                                </div>
+                <div class = "backer d-flex align-items-center">
+                    <div class = "bcard">
+                        <div class="row">
+                            <div class = "col text-center">
+                                <h2 class="head">Welcome to Child Evangelism Ministries</h2>
                             </div>
-                            <div class="row">
-                                <div class = "col text-center">
-                                    <p class="sub">We are so glad you’re here! A little bit about us...</p>
-                                </div>
+                        </div>
+                        <div class="row">
+                            <div class = "col text-center">
+                                <p class="sub">We are so glad you’re here! A little bit about us...</p>
                             </div>
-                            <div class="row">
-                                <div class = "col text-center">
-                                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.</p>
-                                </div>
+                        </div>
+                        <div class="row">
+                            <div class = "col text-center">
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.</p>
                             </div>
-                            <div class="row">
-                                <div class="col text-center">
-                                    <div class="buttons">
-                                        <a href="#" class="button Blue">DECLARATION OF FAITH</a>
-                                        
-                                        <a href="#" class="button Yellow">LATEST NEWSLETTER</a>
-                                    </div>
+                        </div>
+                        <div class="row">
+                            <div class="col text-center">
+                                <div class="buttons">
+                                    <a href="#" class="button Blue">DECLARATION OF FAITH</a>
+                                    
+                                    <a href="#" class="button Yellow">LATEST NEWSLETTER</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- <img class="hero" src="<?php bloginfo('template_directory'); ?>/img/hero.png"> -->
-                <!-- <h3>Children's Stories</h3> -->
                 <div class="children row">
                     <div class="col-sm text-center">
                         <div class="img">
@@ -52,7 +48,14 @@ get_header(); ?>
                     </div>
                 </div>
                 <div class = "row text">
-                    <div class = "col-sm text-center">
+                    <div class = "col-sm text-center trans">
+                            <p class = "story"> Children’s Stories</p>
+                            <p class = "club"> Happy Day Club </p>
+                            <p class = "school"> Vacation Bible School</p>
+                    </div>
+                </div>
+                <div class = "row text">
+                    <div class = "col-sm text-center" style="margin-top: -250px;">
                         <div class = "content">
                             <p class = "children"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                             <p class = "happy"> Happy Day Clubs are after school Bible clubs meeting once a week for one hour from October through March. Happy Day Clubs are taught by teams of trained volunteers. Each week children are taught a Bible lesson, hear the next chapter of an exciting missionary story, sing songs, learn a Bible verse and so much more! </p>
@@ -69,41 +72,46 @@ get_header(); ?>
                         </div>
                     </div>
                 </div>
-                <div class="row title">
-                    <div class = "col-sm">
-                        <h2>Happy Day Club 2020 - 2021</h2>
-                        <h3>Although Happy Day Club isn't able to meet in your school this year we have new ways to enjoy Happy Day Club!</h3>
-                        <p>Happy Day Clubs are after school Bible clubs meeting once a week for one hour from October through March. Happy Day Clubs are taught by teams of trained volunteers. Each week children are taught a Bible lesson, hear the next chapter of an exciting missionary story, sing songs, learn a Bible verse and so much more! </p>
-                    </div>
-                    <div class = "col">
-                        <a href="#" class="club">About Happy Day Club</a>
-                    </div>
-                </div>
-                <img class="clubing" src="<?php bloginfo('template_directory'); ?>/img/club.png">
-                <div class="row">
-                    <div class="col-sm text-center">
-                        <img class="togo" src="<?php bloginfo('template_directory'); ?>/img/togo.png">
-                    </div>
-                    <div class="col-sm righthalf">
-                        <h2>Togo, West Africa</h2>
-                        <h3>Our ministry here at CEM reaches beyond the boarders of Elkhart and LaGrange Counties...</h3>
-                        <img class="map" src="<?php bloginfo('template_directory'); ?>/img/map.png">
-                        <div class="mapbuttons">
-                            <a href="#" class="mapbuttons map">Learn about TogoR</a>
+                <div class = "backer2 d-flex align-items-center">
+                    <div class = "bcard2">
+                        <div class="row">
+                            <div class = "col text-center">
+                                <h2 class="head">Togo, West Africa</h2>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row  Bottom">
-                    <div class ="col text-center">
-                        <h2>Vacation Bible School</h2>
-                        <h3>Lorem ipsum dolor sit amet consectetur</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.</p>
-                        <div class="sign">
-                            <a href="#" class="sign up">Sign up for VBS</a>
+                        <div class="row">
+                            <div class = "col text-center">
+                                <p class="sub">Meet our team 6000 miles away in Togo, West Africa!</p>
+                            </div>
                         </div>
+                        <div class="row">
+                            <div class = "col text-center">
+                                <p class="desc">Our ministry here at CEM reaches beyond the boarders of Elkhart and LaGrange Counties...</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col text-center">
+                                <div class="buttons">
+                                    <a href="#" class="button Blue">LEARN ABOUT TOGO</a>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                    <img class="mapi" src="<?php bloginfo('template_directory'); ?>/img/westafrica.png">
+                </div>
+                <div class = "row">
+                        <div class="col-sm text-center">
+                            <img class="togo" src="<?php bloginfo('template_directory'); ?>/img/ele.png">
+                        </div>
+                        <div class="col-sm righthalf">
+                            <h2>Donate to Child Evangelism Ministeries</h2>
+                            <h3>Help fund our mission. Lorem ipsum dolor sit amet consectetur</h3>
+                            <p class = "subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <div class = "buttons">
+                                <a href="#" class = "button Blue">DONATE</a>
+                            </div>
                     </div>
                 </div>
-                <img class="final" src="<?php bloginfo('template_directory'); ?>/img/final.png">
             </div>
         </section>
     </div>
