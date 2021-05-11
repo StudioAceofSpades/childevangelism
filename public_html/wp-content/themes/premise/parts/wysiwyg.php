ee3654bb16f46d498dc0cfc3f71d0dc27ae48055
antphan98 <antphan98@gmail.com>
2021-05-11 15:05:26 +0000

    added acf modules and made minor changes to home page

---
+++ public_html/wp-content/themes/premise/parts/wysiwyg.php
@@ -0,0 +1,13 @@
+<div class="wysiwyg">
+    <div class="row">
+        <?php if(get_sub_field('width') == 'partial'): ?>
+        <div class="col-md-8 offset-md-2">
+        <?php else: ?>
+        <div class="col">
+        <?php endif; ?>
+            <div class="wysiwyg-content">
+                <?php the_sub_field('content');  ?>
+            </div>
+        </div>
+    </div>
+</div>
