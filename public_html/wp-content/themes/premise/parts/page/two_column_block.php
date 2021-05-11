f89c36f4d67c6e07772272180f59441888ecc9d6
SrishtiAvalakki <spram001@odu.edu>
2021-05-11 20:07:19 +0000

    2 col fixed

--- public_html/wp-content/themes/premise/parts/page/two_column_block.php
+++ public_html/wp-content/themes/premise/parts/page/two_column_block.php
@@ -1,26 +1,34 @@
-<div class="three-column-block" style="background-color: #<?php the_sub_field('background_color'); ?>;">
+<?php
+$class = '';
+if(get_sub_field('background_color') != 'ffffff') {
+    $class = 'background-color';
+}
+?>
+
+
+<div class="two-column-block <?php echo $class; ?> <?php the_sub_field('bottom_margin'); ?>" style="background-color: #<?php the_sub_field('background_color'); ?>;">
     <div class="container">
         <div class="row">
             <?php if (have_rows('column')): ?>
                 <?php while(have_rows('column') ): the_row(); ?>
-                    <div class="col-md-4">
-                            <?php if(get_sub_field('title','options')): ?>
-                                <?php $title = get_sub_field('title'); ?>
-                                <h2 class="title"><?php echo $title ?></h2>
-                            <?php endif; ?>
-                            <div class="col-wrapper">
-                            <?php if(get_sub_field('image','options')): ?>
+                    <div class="col-md-6<?php echo get_sub_field('center_paragraph') ? ' text-center' : '' ?>">
+                        <div class="col-wrapper">
+                            <?php if(get_sub_field('image')): ?>
                                 <?php $img = get_sub_field('image'); ?>
-                                <img src="<?php echo $img['url'] ?>" alt="<?php the_sub_field('title'); ?>" class="img-fluid">
+                                <img src="<?php echo $img ?>" alt="<?php the_sub_field('title'); ?>" class="img-fluid">
                             <?php endif; ?>
-                            <?php if(get_sub_field('description','options')): ?>
+                            <?php if(get_sub_field('title')): ?>
+                                <?php $title = get_sub_field('title'); ?>
+                                <h3 class="title"><?php echo $title ?></h3>
+                            <?php endif; ?>
+                            <?php if(get_sub_field('description')): ?>
                                 <?php $desc = get_sub_field('description'); ?>
                                 <div class="wysiwyg"><?php echo $desc ?></div>
                             <?php endif; ?>
-                            <?php if(get_sub_field('column_button','options')): ?>
+                            <?php if(get_sub_field('column_button')): ?>
                                 <?php $btn = get_sub_field('column_button'); ?>
                                     <div class="buttons">
-                                        <a href="<?php echo $btn['url'];?>" class="button" target="<?php echo $btn['target'];?>"><?php echo $btn['title']; ?></a>
+                                        <a href="<?php echo $btn['url'];?>" class="button blue" target="<?php echo $btn['target'];?>"><?php echo $btn['title']; ?></a>
                                     </div>
                             <?php endif; ?>
                         </div>
