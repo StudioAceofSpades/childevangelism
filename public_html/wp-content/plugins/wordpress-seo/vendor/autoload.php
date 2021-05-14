cc78b27bb37ae1632d883124196f1eb4a84782d6
Ted K. Pagonis <tedlogic@gmail.com>
2021-05-14 20:01:11 +0000

    More plugins updated

--- public_html/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ public_html/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit27d611418e09b72d98b1165f1baaf160::getLoader();
+return ComposerAutoloaderInit5e0b0d75242db2980db07ef62faa199c::getLoader();
