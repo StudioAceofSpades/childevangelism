9216ad39afa0582b92bece72fe7a0d4b9be56654
SrishtiAvalakki <spram001@odu.edu>
2021-05-26 21:03:14 +0000

    hero for vacation bible

--- public_html/wp-content/themes/premise/pages/vacation.php
+++ public_html/wp-content/themes/premise/pages/vacation.php
@@ -4,24 +4,29 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 get_header(); ?>
 
-<?php include(locate_template('parts/hero.php')); ?>
-<div id="vacation">
-    <section class="vacation">
-        <?php 
-        if(have_rows('vacation_content')) :
-            $counter = 0;
-            while(have_rows('vacation_content')) : 
-                the_row(); 
-                $counter++; 
-                $layout = get_row_layout();
-                ?>
-                <div id="section-<?php echo $counter; ?>">
-                    <?php include(locate_template('parts/page/'.$layout.'.php')); ?>
-                </div>
-                <?php
-            endwhile;
-        endif ?>
-    </section>
-</div>
+<body id="top-of-page">
+    <?php include(locate_template('parts/header.php')); ?>
+
+    <div id="vacation">
+    <?php include(locate_template('parts/hero/vacation.php')); ?>
+        <section class="vacation">
+            <?php 
+            if(have_rows('vacation_content')) :
+                $counter = 0;
+                while(have_rows('vacation_content')) : 
+                    the_row(); 
+                    $counter++; 
+                    $layout = get_row_layout();
+                    ?>
+                    <div id="section-<?php echo $counter; ?>">
+                        <?php include(locate_template('parts/page/'.$layout.'.php')); ?>
+                    </div>
+                    <?php
+                endwhile;
+            endif ?>
+        </section>
+    </div>
+    <?php include(locate_template('parts/footer.php')); ?>
+</body>
 
 <?php get_footer(); ?>
