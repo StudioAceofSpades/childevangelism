bf7adb2ee083bce2f1e6b8049ab42eb323e0a2b4
tedlogic <tedlogic@gmail.com>
2021-06-14 20:31:02 +0000

    stub contact page

--- public_html/wp-content/themes/premise/pages/contact.php
+++ public_html/wp-content/themes/premise/pages/contact.php
@@ -4,28 +4,48 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 get_header(); ?>
 
+<?php include(locate_template('parts/hero.php')); ?>
+
 <div id="contact">
-	<div class="content">
-        <?php include(get_stylesheet_directory() . '/parts/hero.php'); ?>
-        <div class='container contact-info'>
-            <div class='row'>
-                <div class='col-sm-4'>
-                    <div class='contact-information'>
-                        <?php the_field('contact_information'); ?>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Get in Touch</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md">
+                <div class="social">
+                    <?php if (get_field('facebook_link', 'options')): ?>
+                        <a href="<?php the_field('facebook_link', 'options'); ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <?php
+                    endif;
+                    if (get_field('youtube_link', 'options')):
+                        ?>
+                        <a href="<?php the_field('youtube_link', 'options'); ?>" target="_blank"><i class="fab fa-youtube"></i></a>
+                    <?php endif; ?>
                 </div>
-                <div class='col-sm-8'>
-                    <div class='map-embed'>
-                        <h2>Find Us Here</h2>
-                        <iframe src="<?php the_field('google_maps_embed_source'); ?>" width="600" height="450" frameborder="0" allowfullscreen></iframe>
-                    </div>
-                    <div class='form-embed'>
-                        <h2>Get In Contact</h2>
-                        <?php cfct_loop(); ?>
-                    </div>
-                </div>
+                <?php if (get_field('email')): ?>
+                    <p><a href="#" target="_blank"><?php the_field('email'); ?></a></p>
+                    <?php
+                endif;
+                if ($phone = get_field('phone')):
+                    $phone_num = str_replace(array(' ','(',')','-','.','+'),'', $phone);
+                    ?>
+                    <p><a href="tel:<?php echo $phone_num; ?>" target="_blank"><?php echo $phone; ?></a></p>
+                    <?php
+                endif;
+                if (get_field('address')):
+                    ?>
+                    <p><?php the_field('address'); ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="col-md">
+                <?php cfct_loop(); ?>
             </div>
         </div>
     </div>
 </div>
+
+
 <?php get_footer(); ?>
