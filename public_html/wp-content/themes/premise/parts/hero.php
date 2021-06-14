ea296b8c5b76e90419fdc2158efe9cd41621eceb
Jon Savage <jon@studioaceofspades.com>
2021-06-14 18:54:11 +0000

    homepage

--- public_html/wp-content/themes/premise/parts/hero.php
+++ public_html/wp-content/themes/premise/parts/hero.php
@@ -1,6 +1,6 @@
 <section id="hero" class="hero">
     <?php if(is_front_page()): ?>
-        <?php include(locate_template('parts/hero/home.php')); ?>
+        <?php include(locate_template('parts/hero/hero-home.php')); ?>
     <?php else: ?>
         <?php include(locate_template('parts/hero/hero-default.php')); ?>
     <?php endif; ?>
