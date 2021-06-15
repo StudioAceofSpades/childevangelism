e75bf4b96b7655aab4c0b5451c6e57df54f4d94e
Jon Savage <jon@studioaceofspades.com>
2021-06-15 19:17:15 +0000

    newsletter

--- public_html/wp-content/themes/premise/parts/page/two_column_block.php
+++ public_html/wp-content/themes/premise/parts/page/two_column_block.php
@@ -6,15 +6,15 @@
                     <div class="col-md-6 col-wrapper">
 
                         <?php if($image = get_sub_field('image')): ?>
-                            <img src="<?php echo $img ?>" alt="<?php the_sub_field('title'); ?>" class="img-fluid">
+                            <img src="<?php echo $image; ?>" alt="<?php the_sub_field('title'); ?>" class="img-fluid">
                         <?php endif; ?>
 
                         <?php if($title = get_sub_field('title')): ?>
-                            <h3 class="title"><?php echo $title ?></h3>
+                            <h3 class="title"><?php echo $title; ?></h3>
                         <?php endif; ?>
 
                         <?php if($desc = get_sub_field('description')): ?>
-                            <div class="wysiwyg"><?php echo $desc ?></div>
+                            <div class="wysiwyg"><?php echo $desc; ?></div>
                         <?php endif; ?>
 
                         <?php if($btn = get_sub_field('column_button')): ?>
