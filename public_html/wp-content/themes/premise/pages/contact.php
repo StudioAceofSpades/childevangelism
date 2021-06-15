ea712d01422a27cc3f0fc9ab2de7f361d5ec39f4
tedlogic <tedlogic@gmail.com>
2021-06-15 16:10:55 +0000

    Contact somewhat styled

--- public_html/wp-content/themes/premise/pages/contact.php
+++ public_html/wp-content/themes/premise/pages/contact.php
@@ -6,42 +6,53 @@ get_header(); ?>
 
 <?php include(locate_template('parts/hero.php')); ?>
 
-<div id="contact">
+<div id="contact" class="bg-dark-blue">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h3>Get in Touch</h3>
+        <div class="contact-container bg-white">
+            <div class="row">
+                <div class="col text-center">
+                    <h3>Get in Touch</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md">
-                <div class="social">
-                    <?php if (get_field('facebook_link', 'options')): ?>
-                        <a href="<?php the_field('facebook_link', 'options'); ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <div class="row">
+                <div class="col-md">
+                    <p class="social">
+                        <?php if (get_field('facebook_link', 'options')): ?>
+                            <a href="<?php the_field('facebook_link', 'options'); ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                            <?php
+                        endif;
+                        if (get_field('youtube_link', 'options')):
+                            ?>
+                            <a href="<?php the_field('youtube_link', 'options'); ?>" target="_blank"><i class="fab fa-youtube"></i></a>
+                        <?php endif; ?>
+                    </p>
+                    <?php if (get_field('email')): ?>
+                        <p>
+                            <label>Email</label><br>
+                            <a href="#" target="_blank"><?php the_field('email'); ?></a>
+                        </p>
                         <?php
                     endif;
-                    if (get_field('youtube_link', 'options')):
+                    if ($phone = get_field('phone')):
+                        $phone_num = str_replace(array(' ','(',')','-','.','+'),'', $phone);
                         ?>
-                        <a href="<?php the_field('youtube_link', 'options'); ?>" target="_blank"><i class="fab fa-youtube"></i></a>
+                        <p>
+                            <label>Phone</label><br>
+                            <a href="tel:<?php echo $phone_num; ?>" target="_blank"><?php echo $phone; ?></a>
+                        </p>
+                        <?php
+                    endif;
+                    if (get_field('address')):
+                        ?>
+                        <p>
+                            <label>Address</label><br>
+                            <?php the_field('address'); ?>
+                        </p>
                     <?php endif; ?>
                 </div>
-                <?php if (get_field('email')): ?>
-                    <p><a href="#" target="_blank"><?php the_field('email'); ?></a></p>
-                    <?php
-                endif;
-                if ($phone = get_field('phone')):
-                    $phone_num = str_replace(array(' ','(',')','-','.','+'),'', $phone);
-                    ?>
-                    <p><a href="tel:<?php echo $phone_num; ?>" target="_blank"><?php echo $phone; ?></a></p>
-                    <?php
-                endif;
-                if (get_field('address')):
-                    ?>
-                    <p><?php the_field('address'); ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="col-md">
-                <?php cfct_loop(); ?>
+                <div class="col-md">
+                    <?php cfct_loop(); ?>
+                </div>
             </div>
         </div>
     </div>
