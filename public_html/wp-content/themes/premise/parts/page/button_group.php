183cd56ab4b6a987abb161e339f335bdd369b44e
tedlogic <tedlogic@gmail.com>
2021-07-08 20:00:12 +0000

    Misc module fixes

--- public_html/wp-content/themes/premise/parts/page/button_group.php
+++ public_html/wp-content/themes/premise/parts/page/button_group.php
@@ -1,31 +1,23 @@
-<?php 
-$left = get_sub_field('align_buttons_left');
-if($left) {
-    $class = "align-buttons-left";
-}
+<?php
+if(get_sub_field('align_buttons_left') == TRUE):
+    $left = "left";
+endif;
 ?>
 
 <div class="button_group">
     <div class="container">
-    <?php if( have_rows('button')): ?>
-        <?php if($left): ?>
-        <div class="buttons row align-buttons-left">
-        <?php else: ?>
-        <div class="buttons row justify-content-center">
-        <?php endif; ?>
-            <?php while( have_rows('button') ) : the_row(); ?>
-                <?php $link = get_sub_field('link'); ?>
-                <?php $color = get_sub_field('color'); ?>
-                
-                <?php if(!$left): ?>
-                <div class="col-lg col-md-6 col-sm-12">
-                <?php endif; ?>
+        <div class="row">
+            <div class="buttons <?php echo $left; ?>">
+                <?php
+                while( have_rows('button') ) : the_row();
+                    $link = get_sub_field('link'); 
+                    $color = get_sub_field('color');
+                    ?>
                     <a href="<?php echo $link['url']; ?>" class="button <?php echo $color; ?>"<?php echo $link['target']; ?>><?php echo $link['title']; ?></a>
-                <?php if(!$left): ?>
-                </div>
-                <?php endif; ?>
-            <?php endwhile; ?>
+                    <?php
+                endwhile;
+                ?>
+            </div>
         </div>
-    <?php endif; ?>
     </div>
 </div>
