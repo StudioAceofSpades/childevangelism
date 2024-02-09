0bdc754ffa6415c1e99293720f32fb632b2fd42e
saosdev <jon@studioaceofspades.com>
2024-02-09 19:23:24 +0000

    missed file add

---
+++ public_html/wp-content/themes/premise/pages/event.php
@@ -0,0 +1,49 @@
+<?php
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+get_header(); ?>
+
+<div id="event">
+ 
+    <div class="event-hero">
+        <div class="event-hero-wrapper">
+            <h2>We invite you to celebrate 75 years of sharing</h2>
+            <h1>The Gift of the Gospel</h1>
+            
+            <span class="suggest">Suggested donation of $25 per ticket or $175 per table of 8</span>
+
+            <div class="event-buttons">
+                <a class="event-button" href="https://www.paypal.com/donate/?hosted_button_id=2GF9DD2DP3U5W" target="_blank">Donate via PayPal</a>
+                <a class="event-button" href="https://www.eventbrite.com/e/75th-anniversary-celebration-tickets-792180452057?aff=ebdsoporgprofile" target="blank">Register Now</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="event-date-time">
+        <div class="date">
+            <h3>Tuesday</h3>
+            <h4>March 26, 2024</h4>
+        </div>
+        <div class="time">
+            <h3>Doors Open</h3>
+            <h4>6:00 PM</h4>
+        </div>
+        <div class="time">
+            <h3>Dinner + Program</h3>
+            <h4>6:30 PM</h4>
+        </div>
+    </div>
+
+    <section id="main">
+        <div class="event-content">
+            <p>We are excited to celebrate 75 years of reaching boys and girls for Jesus here in Elkhart &amp; LaGrange Counties.</p>
+            <p>Join us for a celebration banquet at the Blue Gate in Shipshewana on March 26, 2024.</p>
+            <p>Registration is now OPEN! Get your tickets online at <a href="https://www.eventbrite.com/e/75th-anniversary-celebration-tickets-792180452057?aff=ebdsoporgprofile" target="_blank">eventbrite.com</a> or by calling our office at 574-848-4800.</p>
+            <p>Suggested donation $25 per ticket or $175 per table of 8.</p>
+            <p>Donations may given by mail or through our <a href="https://www.paypal.com/donate/?hosted_button_id=2GF9DD2DP3U5W" target="_blank">PayPal</a>.</p>
+        </div>
+    </section>
+
+</div>
+<?php get_footer(); ?>
